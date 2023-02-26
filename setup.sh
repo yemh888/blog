22cf8544f6e52d64817eb60ae07c672bf1e4af72
yemh <MH_C_Y@163.com>
2023-02-26 17:39:26 +0800

    quiet

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ DATA_DIR="\$(pwd)/public/"
 hexo g > /dev/null 2>&1
 cd \$TMP_DIR && git clone --branch=main \$GIT_DIR data > /dev/null 2>&1
 rsync -av --del --exclude ".git" --exclude ".github" \$DATA_DIR data/ > /dev/null 2>&1
-cd data && git add . && git commit -m "\$(date +%Y%m%d%H%M%S)" && git push > /dev/null 2>&1 && cd \$GIT_DIR
+cd data && git add . >/dev/null 2>&1 && git commit -m "\$(date +%Y%m%d%H%M%S)" >/dev/null 2>&1 && git push > /dev/null 2>&1 && cd \$GIT_DIR
 sudo rm \$TMP_DIR -rf > /dev/null 2>&1
 git push origin main > /dev/null 2>&1
 exit 0
