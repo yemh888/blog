628936fe7e718a478dbf61ecf5253c3c6b2dea87
yemh <MH_C_Y@163.com>
2023-02-26 17:37:20 +0800

    quiet

--- setup.sh
+++ setup.sh
@@ -15,12 +15,12 @@ TMP_DIR=\$(mktemp -d)
 GIT_DIR=\$(pwd)
 DATA_DIR="\$(pwd)/public/"
 
-hexo g > /dev/null
-cd \$TMP_DIR && git clone --branch=main \$GIT_DIR data > /dev/null
-rsync -av --del --exclude ".git" --exclude ".github" \$DATA_DIR data/ > /dev/null
-cd data && git add . && git commit -m "\$(date +%Y%m%d%H%M%S)" && git push > /dev/null && cd \$GIT_DIR
-sudo rm \$TMP_DIR -rf > /dev/null
-git push origin main > /dev/null
+hexo g > /dev/null 2>&1
+cd \$TMP_DIR && git clone --branch=main \$GIT_DIR data > /dev/null 2>&1
+rsync -av --del --exclude ".git" --exclude ".github" \$DATA_DIR data/ > /dev/null 2>&1
+cd data && git add . && git commit -m "\$(date +%Y%m%d%H%M%S)" && git push > /dev/null 2>&1 && cd \$GIT_DIR
+sudo rm \$TMP_DIR -rf > /dev/null 2>&1
+git push origin main > /dev/null 2>&1
 exit 0
 
 EOF
